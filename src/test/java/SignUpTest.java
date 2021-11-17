350af1404562514451eb78e6a25c4dfa976ea682
Faina-Murashko <fainka18@gmail.com>
2021-11-17 23:30:51 +0300

    Проверка регистрации при вводе разных паролей.

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -127,14 +127,48 @@ public class SignUpTest {
          boolean succsessfulRegisterNewUser = driver.findElement(By.cssSelector("[class=confirmation_message]")).isDisplayed();
          Assert.assertTrue(true);
 
-
         //Закрыть браузер.
         driver.quit();
+    }
+
+    @Test //Баг (при вводе данных которые отличаются от основного пароля, в поле подтверждение пароля,
+    // регистрация происходит успешно, но должна быть ошибка с указанием введенного пароля который не сопадает)
+    public void signUpInfoNewUserWrongPassword2(){
+
+        //Открытие страницы https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345");
 
 
+        //Ввод данных нового пользователя;
+        //FirstName
+        WebElement firstNameInput = driver.findElement(By.cssSelector("[name=first_name]"));
+        firstNameInput.sendKeys("Konstantin");
 
+        //LastName
+        WebElement lastNameInput = driver.findElement(By.cssSelector("[name=last_name]"));
+        lastNameInput.sendKeys("Ivanov");
 
+        //Email
+        WebElement emailUserInput = driver.findElement(By.cssSelector("[name=email]"));
+        emailUserInput.sendKeys("something@flag.com");
 
+        //Password
+        WebElement passwordForExample = driver.findElement(By.cssSelector("[name=password1]"));
+        passwordForExample.sendKeys("87654321");
+
+        //ConfirmPassword
+        WebElement confirmPasswordForExample = driver.findElement(By.cssSelector("[name=password2]"));
+        confirmPasswordForExample.sendKeys("87654322");
+
+        //Нажимаем кнопку Register
+        WebElement registerButton = driver.findElement(By.cssSelector("[value=Register]"));
+        registerButton.click();
+
+        //Убедиться в успешной регистрации
+        boolean succsessfulRegisterNewUser = driver.findElement(By.cssSelector("[class=confirmation_message]")).isDisplayed();
+        Assert.fail();
 
 
     }
